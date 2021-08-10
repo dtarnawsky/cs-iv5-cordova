13bea7da45906822238f617bedffead08e513c51
Damian Tarnawsky <damian@ionic.io>
2021-08-10 14:38:28 -0700

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Device } from '@ionic-enterprise/identity-vault';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  async ngOnInit() {
+    const data = await Device.getAvailableHardware();
+    alert(JSON.stringify(data));
+  }
 
 }
